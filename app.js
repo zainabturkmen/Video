4d8a12873b2d56f2350489e7c4a47a7a834644da
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-21 18:52:25 +0430

    setting up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
