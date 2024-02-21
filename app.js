fbca8039feda0719ec7126b1efb71a6e0a57a668
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-21 19:14:17 +0430

    setting up

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container')
