b5bb38889a3efbee0d2f789b50427ba303247d63
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-21 19:29:53 +0430

    addong some changes

--- app.js
+++ app.js
@@ -17,26 +17,3 @@ btn.addEventListener("click", function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// const btn = document.querySelector('.switch-btn');
-// const video = document.querySelector('.video-container');
-
-// btn.addEventListener("click", function(){
-//     if(!btn.classList.contains("slide")){
-//         btn.classList.add("slide");
-//         video.pause();
-//     }else{
-//         btn.classList.remove("slide")
-//         video.play();
-//     }
-// })
